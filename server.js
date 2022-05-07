1e257dc6f586d59e5a4a1a7210d79156ca48c303
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-05-07 18:14:31 +1000

    added static page routes; added routes to index and notes

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
+// import modules, etc
 const express = require("express");
+const path = require("path");
 
+// make variable app for express
 const app = express();
 
+// set the port
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => res.send("set up"));
+// serve static pages
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log(`Server listening on port ${3001}`));
+// homepage route - note taker splash screen
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// notes page route - send to the note taker app
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+// activate the server
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
