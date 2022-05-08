a3942def74e1828641b68be638720cdc03633ca7
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-05-08 17:05:58 +1000

    added wildcard route; added 404 page

--- server.js
+++ server.js
@@ -11,12 +11,19 @@ const PORT = process.env.PORT || 3001;
 // make variable app for express
 const app = express();
 
+// ----------------------------------
+
 // body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // serve static pages
 app.use(express.static(path.join(__dirname, 'public')));
+
+// use router to send to api/notes where all the api functions are
+app.use('/api/notes', notesRouter);
+
+// ----------------------------------
 
 // homepage route - note taker splash screen
 app.get('/', (req, res) => 
@@ -28,7 +35,12 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.use('/api/notes', notesRouter);
+// wildcard route - redirect wrong urls to 404.html error page
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/404.html'))
+);
+
+// ----------------------------------
 
 // activate the server
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
